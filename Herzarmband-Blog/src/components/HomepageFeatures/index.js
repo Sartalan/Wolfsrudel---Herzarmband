0523a3f8b4b3a35bac0eb0b87b2817ac21e19a99
Sarti <elpinguinoalan@gmail.com>
2024-04-21 18:42:28 -0300

    Docs => Improved Design | v0.2v

--- Herzarmband-Blog/src/components/HomepageFeatures/index.js
+++ Herzarmband-Blog/src/components/HomepageFeatures/index.js
@@ -1,60 +1,35 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { IconsListOne, IconsListTwo, IconsListThree } from './../HomepageIcons/FeatureIcons'
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, url }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className="col text--center">
+      <a href={url} target="_blank" id={styles.techA}>
+        <Svg className={styles.techIcon} role="img" />
+        <p className={styles.techText}>{title}</p>
+      </a>
     </div>
   );
 }
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
+    <section className={styles.techSection}>
+      <div className={styles.container}>
+        <h2 className={styles.techTitle}>Tecnologias Usadas:</h2>
+        <hr className={styles.hr} />
         <div className="row">
-          {FeatureList.map((props, idx) => (
+          {IconsListOne.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+        <div className="row">
+          {IconsListTwo.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+        <div className="row">
+          {IconsListThree.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
@@ -62,3 +37,17 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
+/**
+ * 
+ * 
+ * { 
+title: 'Easy to Use',
+Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+description: (
+  <>
+    Docusaurus was designed from the gr
+  </>
+),
+},
+ */
