773404c41a8d4e50a81b1f4e29a0118167729df9
Sarti <elpinguinoalan@gmail.com>
2024-04-28 19:40:08 -0300

    App => React Navigation Added | v0.1

--- App/App.js
+++ App/App.js
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='#000'
+      />
+     <MyTabs/>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen() {
+  return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
     </View>
+  )
+}
+
+function Lol() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> </Text>
+    </View>
+  )
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={Lol} />
+    </Tab.Navigator>
   );
 }
 
